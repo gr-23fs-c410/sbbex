cdbea807ea837d70ac814e6828c9bad1802c3b38
shinis(green) <actionshin@gmail.com>
2024-07-12 11:39:36 +0900

    Question 테이블 생성

---
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,22 @@
+package com.mysite.sbb.question;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+}
