9f8d6bd5d8c380c87d0fe2aac673f1453f826e58
shinis(green) <actionshin@gmail.com>
2024-07-12 14:51:29 +0900

    상세보기 구현

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -14,13 +15,12 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/question")
 public class QuestionController {
 	
-	private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
 
-    @GetMapping("/list")
-    public String list(Model model) {
-    	List<Question> questionList = this.questionRepository.findAll();
-        model.addAttribute("questionList", questionList);
-    	
-        return "question_list";
+	@GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable(name="id", required = false) Integer id) {
+		Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/test/java/com/mysite/sbb/SbbexApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbexApplicationTests.java
@@ -1,13 +1,29 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
 
 @SpringBootTest
 class SbbexApplicationTests {
+	
+	@Autowired
+    private QuestionRepository questionRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+    void testJpa() {
+        Optional<Question> oa = this.questionRepository.findById(1);
+        assertTrue(oa.isPresent());
+    }
 }
